6d0f5824a72369dc3679242a4ef98971247db486
ItsKirby69 <117233443+ItsKirby69@users.noreply.github.com>
2022-11-26 19:15:57 -0500

    Update Minedusty.java

--- src/minedusty/Minedusty.java
+++ src/minedusty/Minedusty.java
@@ -28,7 +28,7 @@ public class Minedusty extends Mod{
             //show dialog upon startup
             Time.runTask(10f, () -> {
                 BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
+                dialog.cont.add("Behold").row();
                 //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
                 dialog.cont.image(Core.atlas.find("MineDusty-frog")).pad(20f).row();
                 dialog.cont.button("I see", dialog::hide).size(100f, 50f);
