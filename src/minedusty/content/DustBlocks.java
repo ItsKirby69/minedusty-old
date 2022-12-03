b45fd44e84cbff3bd380e3caa1c09ab0fd4d5cd5
ItsKirby69 <117233443+ItsKirby69@users.noreply.github.com>
2022-12-03 12:47:35 -0500

    test

--- src/minedusty/content/DustBlocks.java
+++ src/minedusty/content/DustBlocks.java
@@ -35,7 +35,7 @@ public class DustBlocks {
 	//Walls
 	//Drills
 	//Props
-	largeBoulder;// aliveTree;
+	largeBoulder, aliveTree;
 	//add more categories
 
 	public static void load(){
@@ -113,7 +113,7 @@ public class DustBlocks {
 		}};
 	
 		//deco blocks
-		//aliveTree = new TreeBlock("live-tree");
+		aliveTree = new TreeBlock("live-tree");
 
 		
 	}
